f354ac68963d738c311f12322be7ff32975dd6d8
aronfan <aronfan@vip.qq.com>
2019-09-10 13:25:48 +0800

    modify proto convert target dir

--- protocol/convert_win.bat
+++ protocol/convert_win.bat
@@ -2,4 +2,4 @@
 
 chcp 65001
 
-%GOPATH%\bin\protoc.exe --plugin=protoc-gen-go=%GOPATH%\bin\protoc-gen-go.exe --go_out=..\core\proto\getcd\ getcd.proto
+%GOPATH%\bin\protoc.exe --plugin=protoc-gen-go=%GOPATH%\bin\protoc-gen-go.exe --go_out=plugins=grpc:..\core\protocol\getcd\ getcd.proto

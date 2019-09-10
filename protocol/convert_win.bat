fa2164b731badd65d0bf90c6a2526f4615cf41dd
aronfan <aronfan@vip.qq.com>
2019-09-10 16:12:50 +0800

    add errno used by services

--- protocol/convert_win.bat
+++ protocol/convert_win.bat
@@ -2,4 +2,8 @@
 
 chcp 65001
 
+rem convert the errno
+%GOPATH%\bin\protoc.exe --plugin=protoc-gen-go=%GOPATH%\bin\protoc-gen-go.exe --go_out=..\core\protocol\gerrno\ gerrno.proto
+
+rem convert the getcd
 %GOPATH%\bin\protoc.exe --plugin=protoc-gen-go=%GOPATH%\bin\protoc-gen-go.exe --go_out=plugins=grpc:..\core\protocol\getcd\ getcd.proto

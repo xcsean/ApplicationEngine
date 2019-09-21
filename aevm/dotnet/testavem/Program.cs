0abd66544b215eda52f949680164c4e589f74576
Sean Chen <xc.sean@hotmail.com>
2019-09-21 15:54:18 +0800

    profile grpc

--- aevm/dotnet/testavem/Program.cs
+++ aevm/dotnet/testavem/Program.cs
@@ -17,16 +17,30 @@ namespace testavem
             client.connect("47.103.158.190:17000");
 
             query_global_config_req cat = new query_global_config_req();
-            cat.Categories.Add("");
+            cat.Categories.Add("global");
+
+            long start = DateTime.UtcNow.Ticks;
 
             var queryConfigRes = client.getcd.QueryGlobalConfig(cat);
-            Console.WriteLine("queryConfigRes res [" + queryConfigRes.Result + "] entries count[" + queryConfigRes.Entries.Count + "]");
+
+            long qctN = DateTime.UtcNow.Ticks;
+            long qct = qctN - start;
 
             var protolimit = client.getcd.QueryProtoLimit(new query_proto_limit_req { });
-            Console.WriteLine("protolimit res [" + protolimit.Result + "] entries count[" + protolimit.Entries.Count + "]");
+
+            long pltN = DateTime.UtcNow.Ticks;
+            long plt = pltN - qctN;
 
             var registryRes = client.getcd.QueryRegistry(new query_registry_req { });
-            Console.WriteLine("registryRes res [" + registryRes.Result + "] servers count[" + registryRes.Servers.Count +"] services count["+ registryRes.Services.Count+ "]");
+
+            long rrtN = DateTime.UtcNow.Ticks;
+            long rrt = rrtN - pltN;
+
+            Console.WriteLine("queryConfigRes tm:"+ qct + " res [" + queryConfigRes.Result + "] entries count[" + queryConfigRes.Entries.Count + "] entries:"+ queryConfigRes.Entries.ToString());
+
+            Console.WriteLine("protolimit tm:"+ plt + "res [" + protolimit.Result + "] entries count[" + protolimit.Entries.Count + "]");
+
+            Console.WriteLine("registryRes tm:"+ rrt + " res [" + registryRes.Result + "] servers count[" + registryRes.Servers.Count +"] services count["+ registryRes.Services.Count+ "]");
         }
     }
 }

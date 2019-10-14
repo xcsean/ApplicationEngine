0cd54d5ea39c9060b8a26b0ca2911835f10b3766
aronfan <aronfan@vip.qq.com>
2019-10-14 21:56:47 +0800

    update sql stmt

--- core/shared/mysql/mysql_test.go
+++ core/shared/mysql/mysql_test.go
@@ -6,14 +6,14 @@ import (
 	"testing"
 )
 
-func TestQueryCB(t *testing.T) {
+func TestQuery(t *testing.T) {
 	pool, err := New("root", "123456", "192.168.95.182", "3306", "app_ghost_1")
 	if err != nil {
 		t.Errorf("mysql init failed: %s", err.Error())
 		return
 	}
 
-	stmt := fmt.Sprintf("SELECT * FROM t_userasset WHERE ghostid=%d AND uuid=%d", 1, 10001)
+	stmt := fmt.Sprintf("SELECT ghostid, uuid, revision, asset FROM t_userasset WHERE ghostid=%d AND uuid=%d", 1, 10001)
 	err = pool.Query(stmt, func(rows *sql.Rows) error {
 		for rows.Next() {
 			var ghostID uint32 = 0
@@ -22,7 +22,7 @@ func TestQueryCB(t *testing.T) {
 			var asset []byte
 			err := rows.Scan(&ghostID, &uuid, &revision, &asset)
 			if err == nil {
-				t.Logf("ghostid=%d, uuid=%d, revision=%d, asset len=%d", ghostID, uuid, revision, len(asset))
+				t.Logf("ghostid=%d, uuid=%d, revision=%d, assetLen=%d", ghostID, uuid, revision, len(asset))
 			} else {
 				t.Errorf("scan failed: %s", err.Error())
 			}

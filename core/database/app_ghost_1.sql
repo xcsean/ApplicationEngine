c9b3de3237ac30ccd86f215e9f8475423d92a36b
aronfan <aronfan@vip.qq.com>
2019-10-15 15:58:10 +0800

    update the ghost sql script

--- core/database/app_ghost_1.sql
+++ core/database/app_ghost_1.sql
@@ -7,9 +7,9 @@ DROP TABLE IF EXISTS `t_userasset`;
 CREATE TABLE `t_userasset`  (
   `ghostid` int(10) UNSIGNED NOT NULL COMMENT 'ghost id',
   `uuid` bigint(20) UNSIGNED NOT NULL COMMENT 'user unique id',
-  `revision` bigint(20) UNSIGNED NOT NULL COMMENT 'revision',
   `lockerid` bigint(20) UNSIGNED NOT NULL COMMENT 'locker id',
   `expiredtime` bigint(20) NOT NULL COMMENT 'expired time',
+  `revision` bigint(20) UNSIGNED NOT NULL COMMENT 'revision',
   `asset` mediumblob COMMENT 'asset data',
   PRIMARY KEY (`ghostid`, `uuid`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
